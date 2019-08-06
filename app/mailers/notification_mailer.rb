33b71ed0a378a70ccdbae1cf312f5692856eb54a
kv-2 <str_token@mail.ru>
2019-08-06 21:38:18 +0300

    end it braik

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,13 +1,7 @@
 class NotificationMailer < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.notification_mailer.new_account.subject
-  #
-  def new_account
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def new_account(user)
+    @user = user
+    mail(to: user.email, subject: "Acount #{user.name} is active")
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
+  
   validates :name, presence: true
   validates :email, presence: true, format: {with: /\A(^@\s)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
+
+  after_save :send_welcome_email
+  
+  private
+
+   def send_welcome_email
+    NotificationMailer.new_account(self).deliver_later
+   end
 end
